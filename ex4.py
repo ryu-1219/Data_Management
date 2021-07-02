5e0b180eaf0dedb48074b4e678d6d04c4e3242e7
羽賀琉登 <ryuto@wlan-napt-002.u-aizu.ac.jp>
2021-07-02 11:39:40 +0900

    Initial revision

---
+++ ex4.py
@@ -0,0 +1,12 @@
+import random
+
+print('Rolling the dice...')
+
+count = 0
+
+for i in range(2):
+    num_1 = random.randrange(6)+1
+    count += num_1
+    print('Die {}:{}'.format(i+1, num_1))
+
+print('Total value: {}'.format(count))
